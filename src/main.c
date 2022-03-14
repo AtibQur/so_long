630ecd8d3ae1e2926fce756f6eab30c3f689228b
Haseeb Qureshie <hqureshi@f0r2s3.codam.nl>
2022-03-14 16:49:09 +0100

    change file

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hqureshi <hqureshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 10:51:51 by hqureshi          #+#    #+#             */
-/*   Updated: 2022/03/14 13:05:14 by hqureshi         ###   ########.fr       */
+/*   Updated: 2022/03/14 16:48:07 by hqureshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,19 @@ int	main(void)
 {
 	t_data	data;
 	t_data	img;
-
+	int i = 450;
+	int j = 450;
 	data.mlx = mlx_init();
 	img.img = mlx_new_image(data.mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, \
 	&img.line_length, &img.endian);
 	data.mlx_win = mlx_new_window(data.mlx, 1920, 1080, "So_long");
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	while (i <= 950)
+	{
+		my_mlx_pixel_put(&img, i, j, 0xf5f5f5);
+		i++;
+		j++;
+	}
 	mlx_put_image_to_window(&data.mlx, data.mlx_win, img.img, 0 ,0);
 	mlx_loop(data.mlx);
 }
--- src/so_long.h
+++ src/so_long.h
@@ -6,7 +6,7 @@
 /*   By: hqureshi <hqureshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 10:53:29 by hqureshi          #+#    #+#             */
-/*   Updated: 2022/03/14 13:01:04 by hqureshi         ###   ########.fr       */
+/*   Updated: 2022/03/14 14:59:11 by hqureshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,16 @@
 # include "../libft/libft.h"
 # include "../ft_printf/ft_printf.h"
 # include "../mlx/mlx.h"
+
+typedef	enum {
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+} keys;
 
 typedef struct s_data {
 	void	*img;
